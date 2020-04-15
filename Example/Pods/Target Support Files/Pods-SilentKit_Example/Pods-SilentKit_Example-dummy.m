6117981b239b048640cf200717d103ec1b4d0b58
kailen.ni <kailen.ni@51job.com>
2020-04-15 12:35:43 +0800

    first
    Signed-off-by: kailen.ni <kailen.ni@51job.com>

---
+++ Example/Pods/Target Support Files/Pods-SilentKit_Example/Pods-SilentKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SilentKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SilentKit_Example
+@end
---
+++ Example/Pods/Target Support Files/SilentKit/SilentKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SilentKit : NSObject
+@end
+@implementation PodsDummy_SilentKit
+@end
